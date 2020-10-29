61c5939e742ab029acceb2efb8ce934d84ad09b8
lfj <lifengjiao@longguikeji.com>
2020-10-29 19:36:01 +0800

    init

---
+++ src/admin/common/Cascader/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-cascader
+    v-model="value"
+    :options="options"
+    :props="{expandTrigger: 'hover'}"
+    @change="handleChange"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import CascaderState from './CascaderState'
+
+@Component({
+  name: 'Cascader',
+  components: {}
+})
+export default class extends Vue {
+  @Prop({ required: true }) state!: CascaderState;
+
+  actionHandler(actionName: string) {
+    console.log(actionName)
+    // runAction(actionName)
+  }
+}
+</script>
