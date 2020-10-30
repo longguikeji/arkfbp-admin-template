3a5a8079b918cc5d8507f5441aa8696c55706461
Jennifer <lifengjiao@longguikeji.com>
2020-10-30 18:26:03 +0800

    merge

--- src/admin/common/Form/Select/SelectState.ts
+++ src/admin/common/Form/Select/SelectState.ts
@@ -1,9 +1,11 @@
+import OptionType from './OptionType'
 export default interface SelectState {
   value: any; // 绑定值
   type?: {
     type: string; // // el-group-option && el-option
     default: 'only';
   };
+  options?: Array<OptionType>;
   multiple?: boolean; // 是否多选
   disabled?: boolean; // 是否禁用
   valueKey?: string; //作为 value 唯一标识的键名，绑定值为对象类型时必填
--- src/admin/common/Form/Select/index.vue
+++ src/admin/common/Form/Select/index.vue
@@ -1,26 +1,47 @@
 <template>
-  <option-only
-    v-if="state.type !== 'group'"
-    :state="state"
-  />
-  <option-group
-    v-else
-    :state="state"
-  />
+  <el-select
+    v-model="state.value"
+    :placeholder="state.placeholder"
+    :multiple="state.multiple"
+    :clearable="state.clearable"
+    :disabled="state.disabled"
+    :size="state.size"
+  >
+    <template v-if="state.type === 'group'">
+      <el-option-group
+        v-for="(group, groupIndex) in state.options"
+        :key="groupIndex"
+        :label="group.label"
+        :disabled="state.disabled"
+      >
+        <el-option
+          v-for="(item, index) in group.options"
+          :key="index"
+          :label="item.label"
+          :value="item.value"
+          :disabled="item.disabled"
+        />
+      </el-option-group>
+    </template>
+    <template v-else>
+      <el-option
+        v-for="(item, index) in state.options"
+        :key="index"
+        :label="item.label"
+        :value="item.value"
+        :disabled="item.disabled"
+      />
+    </template>
+  </el-select>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
-import OptionOnly from './Option/index.vue'
-import OptionGroup from './OptionGroup/index.vue'
 import SelectState from './SelectState'
 
 @Component({
-  name: 'Select',
-  components: {
-    OptionOnly,
-    OptionGroup
-  }
+  name: 'OptionModule',
+  components: {}
 })
 export default class extends Vue {
   @Prop({ required: true }) state!: SelectState;
