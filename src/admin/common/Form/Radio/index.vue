9ea76730d9269bdfde6071d2c6e6077ad213e185
lfj <lifengjiao@longguikeji.com>
2020-10-30 17:31:34 +0800

    init radio

---
+++ src/admin/common/Form/Radio/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <span v-if="state.readonly">{{ state.value }} </span>
+  <el-radio
+    v-else
+    v-model="state.value"
+    :label="state.label"
+    :disabled="state.disabled"
+    :border="state.border"
+    :size="state.size"
+    :name="state.name"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import RadioState from './RadioState'
+
+@Component({
+  name: 'Radio',
+  components: {}
+})
+export default class extends Vue {
+  @Prop({ required: true }) state!: RadioState;
+}
+</script>
