78fc2890a903c1211dedb6595d7320deef30a458
longgui-penglei <penglei@longguikeji.com>
2020-11-09 11:35:31 +0800

    delete-tablepage-config-extra-code

--- src/admin/TablePage/index.vue
+++ src/admin/TablePage/index.vue
@@ -5,20 +5,6 @@
     </div>
     <Table :state="state.table" />
     <Pagination :state="state.pagination" />
-    <!-- <TagComponent :state="state.tag" /> -->
-    <Tag :state="state.tag" />
-    <Dialog
-      v-for="(dialogName, dialogIndex) in state.dialogs"
-      :key="dialogIndex"
-      :state="state.dialogs[dialogIndex]"
-      :content="state.form"
-    />
-    <NavMenu :state="state.navs" />
-    <Alert :state="state.alert" />
-    <Tree :state="state.tree" />
-    <Steps :state="state.steps" />
-    <Dropdown :state="state.dropdown" />
-    <Tabs :state="state.tabs" />
   </Card>
 </template>
 
@@ -30,14 +16,6 @@ import Table from '@/admin/common/data/Table/index.vue'
 import Button from '@/admin/common/Button/index.vue'
 import Form from '@/admin/common/Form/index.vue'
 import Pagination from '@/admin/common/data/Pagination/index.vue'
-import Tag from '@/admin/common/data/Tag/index.vue'
-import Dialog from '@/admin/common/Others/Dialog/index.vue'
-import Tree from '@/admin/common/data/Tree/index.vue'
-import Alert from '@/admin/common/Notice/Alert/index.vue'
-import Steps from '@/admin/common/Navigation/Steps/index.vue'
-import Dropdown from '@/admin/common/Navigation/Dropdown/index.vue'
-import Tabs from '@/admin/common/Navigation/Tabs/index.vue'
-import NavMenu from '@/admin/common/Navigation/NavMenu/index.vue'
 @Component({
   name: 'FromPage',
   components: {
@@ -45,15 +23,7 @@ import NavMenu from '@/admin/common/Navigation/NavMenu/index.vue'
     Card,
     Button,
     Table,
-    Pagination,
-    Tag,
-    Dialog,
-    Tree,
-    Alert,
-    Steps,
-    Dropdown,
-    Tabs,
-    NavMenu
+    Pagination
   }
 })
 export default class extends Vue {
