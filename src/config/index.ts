86d0c21a28702c10fc26565cf744bb4cb6d3db6f
yuhannn0814 <yuhan@longguikeji.com>
2020-11-12 10:38:57 +0800

    commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -28,22 +28,23 @@ export class Config {
 
   private getConfig(): any {
     const meta = this._serveconfig[this._current].meta
+    const api = this._serveconfig[this._current].api
     const tempMeta: any = {}
+    const tempApi: any = {}
+    const a = this._viewconfig
     debugger
-    const walkServeConfig = (data: any, map: any) => {
+    const walkMeta = (data: any, map: any) => {
       Object.keys(data).forEach((e: any) => {
         const type = data[e].type
 
-        if (type.array) {
-          // TODO
-        }
-
         if (type.object) {
-          Object.keys(type.object).forEach(m => {
-            const value = type.object[e]
-            if (value) {
-
-              walkServeConfig(this._serveconfig[type.model_object.config_path].meta, map[m])
+          Object.keys(type.object).forEach(k => {
+            const value = type.object[k]
+            if (value.includes('.')) {
+              map[e] = {}
+              walkMeta(this._serveconfig[value.split('.')[0]].meta, map[e])
+            } else {
+              walkMeta(data[value].meta, map[e])
             }
           })
         } else {
@@ -58,8 +59,66 @@ export class Config {
       })
     }
 
-    walkServeConfig(meta, tempMeta)
+    const walkApi = (data: any, map: any) => {
+      Object.keys(data).forEach((e: any) => {
+        let index: any = {}
+        if (data[e].index) {
+          index = getSrcMeta(data[e].index)
+        }
+        Object.keys(data[e]).forEach((k: any) => {
+          if (data[e][k].type) {
+            if (map[data[e][k].type]) {
+              map[data[e][k].type].push({
+                url: e,
+                index,
+                label: data[e][k].name,
+                action: data[e][k].type,
+                request: getSrcMeta(data[e][k].request)
+              })
+            } else {
+              map[data[e][k].type] = []
+              map[data[e][k].type].push({
+                url: e,
+                index,
+                label: data[e][k].name,
+                action: data[e][k].type,
+                request: getSrcMeta(data[e][k].request)
+              })
+            }
+          }
+        })
+      })
+    }
 
+    const getSrcMeta = (params: any) => {
+      const map: any = {}
+      Object.keys(params).forEach((p: any) => {
+        if (typeof params[p] === 'object') {
+          if (params[p].src.includes('.')) {
+            const arr = params[p].src.split('.')
+            const data = this._serveconfig[arr[0]].meta[arr[1]]
+            map[p] = {
+              label: data.title,
+              state: {
+                value: '',
+                ...(Object.values(data.type)[0] as Object)
+              }
+            }
+          }
+        } else {
+          map[p] = {
+            state: {
+              value: ''
+            }
+          }
+        }
+      })
+      return map
+    }
+
+    walkMeta(meta, tempMeta)
+    walkApi(api, tempApi)
+    debugger
     const config = cloneDeep(this._viewconfig)
 
     if (this._viewconfig.dialogs) {
@@ -88,14 +147,5 @@ export class Config {
     }
 
     return config
-  }
-
-  private getData(v) {
-    if (v.includes('.')) {
-
-    } else {
-      map[e] = {}
-      walkServeConfig(this._serveconfig[type.model_object.config_path].meta, map[e])
-    }
   }
 }
