e9b65d4c6c84a65be12edaccf857ecae92f8497b
yuhan <yuhan@longguikeji.com>
2020-11-17 17:34:38 +0800

    rebase

--- src/admin/common/Button/ButtonArray/ButtonArrayState.ts
+++ src/admin/common/Button/ButtonArray/ButtonArrayState.ts
@@ -1,6 +1,6 @@
 import ButtonState from '../ButtonState'
-import SwitchButton from '../SwitchButton/SwitchButtonState'
+import SwitchButtonState from '../SwitchButton/SwitchButtonState'
 
 export default interface ButtonArrayState {
-  [index:number]:[ButtonState|SwitchButton]
+  [index:number]:[ButtonState|SwitchButtonState]
 }
