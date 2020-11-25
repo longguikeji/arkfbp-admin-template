ca3a9d6e1327d8c1dfd856a5f3f2e037964ab0be
Jennifer <lifengjiao@longguikeji.com>
2020-11-25 11:56:18 +0800

    页面配置

--- src/flows/selection/nodes/retrieve.ts
+++ src/flows/selection/nodes/retrieve.ts
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// import { APITokenNode } from '@/nodes/APITokenNode'
-
-// export class Retrieve extends APITokenNode {
-//   async run() {
-//     this.url = this.inputs.url
-//     this.method = this.inputs.method
-//     this.inputs.params.ids = this.inputs.params.ids.map((item: any) => item.id)
-//     this.params = this.inputs.params
-//     this.$state.commit((state: any) => {
-//       state.client = this.inputs.client
-//       state.clientServer = this.inputs.clientServer
-//     })
-//     const outputs = await super.run()
-//     return outputs
-//   }
-// }
-=======
 import { TokenAPINode } from '@/arkfbp/nodes/tokenAPINode'
 
 export class Retrieve extends TokenAPINode {
@@ -32,4 +14,3 @@ export class Retrieve extends TokenAPINode {
     return outputs
   }
 }
->>>>>>> e4a62de07d0b0f7cace761a609d07fa1686ff80a
